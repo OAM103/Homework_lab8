81ed85ba98a6a7636762fa7eacb276b7d09cd5ec
OAM103 <113307610+OAM103@users.noreply.github.com>
2022-11-21 10:46:54 +0200

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,16 @@
+package com.metanit;
+
+public class Ex2{
+    public static void main(String[] args) {
+        print_range(1, 10);
+    }
+
+    private static void print_range(int begin, int N) {
+
+        if (begin <= N) {
+            System.out.print(begin + " ");
+            begin ++;
+            print_range(N, begin);
+        }
+    }
+}
---
+++ Ex3.java
@@ -0,0 +1,24 @@
+package com.metanit;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 3;
+        print_range(A, B);
+    }
+    private static void print_range(int A, int B) {
+
+        if (A < B) {
+            print_range(A, B-1);
+            System.out.print(B + " ");
+
+        }
+        else if (A > B) {
+            System.out.print(A + " ");
+            print_range(A-1, B);
+        }
+        else{
+            System.out.print(A + " ");
+        }
+    }
+}
